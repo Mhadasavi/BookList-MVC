74cc78249a28acb484056a2fa4ff86373eae07c8
Upkar <uchaudhary57@gmail.com>
2021-08-02 11:35:43 +0530

    Add project files.

---
+++ BookList MVC/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_MVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String BookName{ get; set; }
+        public String Author{ get; set; }
+        public String ISBN{ get; set; }
+    }
+}
